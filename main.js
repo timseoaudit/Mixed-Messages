89d0ed9efffe8c2e5749b40b53c75ed1d385bab9
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-08-23 13:23:18 +0100

    finish up program

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-/* input from user maybe function that takes a string as an entry 
-create random message from building blocks Arrays with X amount of answers
-output to user */ 
 
-const questionAnswerGame = string => {
-    possibleAnswers = ['Not today', 'Maybe', 'Yes', 'No', 'Ask again later', 'I don\'t know', 'I don\'t think so', 'I think so', 'I\'m not sure, I\'m sure', 'I\'m positive', 'I\'m negative', 'I\'m neutral', 'I\'m not neutral', 'I\'m not positive']
-    return possibleAnswers[Math.floor(Math.random() * possibleAnswers.length)]
+const questionAnswerGame = string => { // call function with a string the string is the question
+    possibleAnswers = ['Not today', 'Maybe', 'Yes', 'No', 'Ask again later', 'I don\'t know', 'I don\'t think so', 'I think so', 'I\'m not sure about this', 'I\'m sure', 'I\'m positive about this', 'I\'m negative about this', 'I\'m neutral about this', 'I\'m not neutral about this, but I also can not give you an answer for this ', 'I\'m not positive about this ']
+    // possibleAnswers has all answeres prepared that the program give out
+    console.log(` Your question was: ${string}. And I tell you that ${possibleAnswers[Math.floor(Math.random() * possibleAnswers.length)]}.`)
+    //log the answer in the console and will return a string that repeat the question and than give one of the random answers.
 }
 
-console.log(questionAnswerGame('Will I win the lottery?'))
+questionAnswerGame('Will we go home today?')
+// call the function with a question as a string.
